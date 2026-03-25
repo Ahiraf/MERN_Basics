5b98d9b06227fa8b7618d9f0038490672442780f
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2026-03-25 11:25:17 +0600

    Enhance documentation on sync vs async file methods
    Added a comparison table for asynchronous and synchronous file methods.

--- 01_Node/06_SynchronousModule.js
+++ 01_Node/06_SynchronousModule.js
@@ -17,7 +17,16 @@ Core Synchronous FS Methods :
 3) fs.renameSync() (Equivalent to rename)
 4) fs.unlinkSync() (Equivalent to unlink 
 5) fs.existsSync() (Equivalent to exists)
-*/
 
-/* Quick Tip: While these are great for scripts and CLI tools, try to avoid using the Sync methods in a high-traffic web server,
+
+Feature                           Asynchronous (fs.writeFile)                                  Synchronous (fs.writeFileSync)
+-----------------------------------------------------------------------------------------------------------------------------------
+Arguments                      Data + Callback Function                                              Data only
+Execution                   Non-blocking: Code continues while writing                   Blocking: Script waits until write finishes
+Error Handling                Handled via (err) in callback                                  Handled via try...catch blocks
+Return Value                       Returns undefined                                         Returns undefined (or data for read)
+
+
+
+ Quick Tip: While these are great for scripts and CLI tools, try to avoid using the Sync methods in a high-traffic web server,
 as they "block" the entire process until the operation finishes!  */
