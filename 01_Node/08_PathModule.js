37e2d3feffc2d64f4d52d75e121dc47c04f002a8
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2026-03-25 11:27:10 +0600

    Add files via upload

---
+++ 01_Node/08_PathModule.js
@@ -0,0 +1,8 @@
+const path = require('path');
+console.log(path);
+
+const extensionName = path.extname("index.html"); // It returns the extension name of the file.
+console.log(extensionName); // It will show ".html" which is the extension of this file.
+
+const joinName = path.join()
+
