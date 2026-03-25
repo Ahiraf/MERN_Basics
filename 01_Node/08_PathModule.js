e39fe984dfe6062d9b53b11fba7b167ea7efeae5
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2026-03-25 11:43:52 +0600

    Update 08_PathModule.js

--- 01_Node/08_PathModule.js
+++ 01_Node/08_PathModule.js
@@ -4,5 +4,14 @@ console.log(path);
 const extensionName = path.extname("index.html"); // It returns the extension name of the file.
 console.log(extensionName); // It will show ".html" which is the extension of this file.
 
-const joinName = path.join()
+/*
+const joinName = path.join(__dirname + "/Cycle"); // It joins the root directory with "Cycle" folder to create a new path.
+console.log(joinName);
+*/
+
+
+const joinName = path.join(__dirname + "../Cycle"); 
+console.log(joinName); // Now it will show the path of the parent directory of the current file.
+
+
 
